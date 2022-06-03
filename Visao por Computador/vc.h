c5c7d3df6a285fe1f0847a33f34ab58e1803e601
IMTorrres <82275287+IMTorrres@users.noreply.github.com>
2022-06-03 22:35:51 +0100

    sem apagar

--- Visao por Computador/vc.h
+++ Visao por Computador/vc.h
@@ -40,45 +40,19 @@ typedef struct
 	int label;				 // Etiqueta
 } OVC;
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//                    PROT�TIPOS DE funcoes
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// funcoes: ALOCAR E LIBERTAR UMA IMAGEM
 IVC *vc_image_new(int width, int height, int channels, int levels);
 IVC *vc_image_free(IVC *image);
-int vc_gray_negative(IVC *srcdst);
-// funcoes: LEITURA E ESCRITA DE IMAGENS (PBM, PGM E PPM)
 IVC *vc_read_image(char *filename);
 int vc_write_image(char *filename, IVC *image);
-int vc_read_to_gray(IVC *filename, IVC *image);
-int vc_gray_negative(IVC *srcdst);
-int High_Threshold(IVC *src, IVC *dst, int Threshold);
 int vc_binary_dilate(IVC *src, IVC *dst, int kernel);
-int vc_etiquetagem(IVC *src, IVC *dst);
-
-
 OVC *vc_binary_blob_labelling(IVC *src, IVC *dst, int *nlabels);
 int vc_binary_blob_info(IVC *src, OVC *blobs, int nblobs);
-int vc_gray_histogram_show(IVC *src, IVC *dst);
-int vc_gray_histogram_equalization(IVC* srcdst);
-int vc_gray_to_binary(IVC* srcdst, int Threshold);
-int vc_gray_to_binary_midpoint(IVC* src, IVC* dst);
-int vc_gray_negative(IVC* srcdst);
-int vc_read_to_gray(IVC* src, IVC* dst); 
-int vc_rgb_negative(IVC* srcdst);
 int vc_bgr_rgb(IVC* srcdst);
-int vc_read_to_gray(IVC* src, IVC* dst);
-int vc_gray_to_binary_global_mean(IVC* src, IVC* dst);
-int vc_etiquetagem(IVC* src, IVC* dst);
-int vc_rgb_to_gray(IVC* src, IVC* dst);
-int vc_rgb_to_hsv1(IVC* src);
 int vc_hsv_segmentation(IVC* src, IVC* dst, int hmin, int hmax, int smin,int smax, int vmin, int vmax);
 OVC* vc_binary_blob_labelling(IVC* src, IVC* dst, int* nlabels);
 int vc_binary_blob_info(IVC* src, OVC* blobs, int nblobs);
 int vc_binary_erode(IVC* src, IVC* dst, int kernel);
-int vc_binary_open(IVC* src, IVC* dst, int kernelErosion, int kernelDilation);
-int vc_binary_close(IVC* src, IVC* dst, int kernelDilation, int kernelErosion);
 int rgb_hsv(IVC* src, IVC* dst);
 int vc_change_color(IVC* src, IVC* dst);
 int px_mm(int px);
